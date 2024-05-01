c6f260a9d157aaf17fe1e82614842713d206e9a8
Thinh Huynh <htthinh1999@gmail.com>
2024-05-01 11:49:15 +0700

    Init Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "project_id" {
+  default = "keycode-mon"
+
+}
+
+variable "region" {
+  default = "asia-southeast1"
+}
---
+++ terraform/version.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.27.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.27.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+  }
+}
