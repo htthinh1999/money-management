9435df127aa55ac24ad5d91ed6900f6e8f3b12be
Thinh Huynh <htthinh1999@gmail.com>
2024-06-02 11:30:21 +0700

    Add more store daily money

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ import requests
 import utils.logger as logger
 import database.database as database
 import repositories.history_repository as history_repository
+import repositories.daily_repository as daily_repository
 from flask import Flask, request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
@@ -144,6 +145,12 @@ def process_gmail_data(gmail_data):
             cost = cost_convert(get_value_from_mail_html_with_i_tag(mail_html, "Amount"))
             details_of_payment = get_value_from_mail_html_with_i_tag(mail_html, "Details of Payment")
             beneficiary_name = get_value_from_mail_html_with_i_tag(mail_html, "Beneficiary Name")
+            trans_date_time = get_value_from_mail_html_with_i_tag(mail_html, "Trans. Date, Time")
+            trans_date = date_from_trans_date_time(trans_date_time)
+            trans_time = date_from_trans_date_time(trans_date_time, trans_time=True)
+            # store daily money
+            daily_repository.store_daily_money(cost, beneficiary_name, details_of_payment, trans_date, trans_time)
+            # send telegram message
             message = f"{subject} - {beneficiary_name}: <b>{cost}</b> VND\n{details_of_payment}"
             send_telegram(message)
 
@@ -158,6 +165,8 @@ def get_value_from_mail_html_with_i_tag(mail_html, tag_value):
     tag_end_pos = mail_html.find('</td>', tag_start_pos)
     # get tag value
     tag_value = mail_html[tag_start_pos+1:tag_end_pos]
+    # trim start and end space
+    tag_value = tag_value.strip()
     return tag_value
 
 def cost_convert(cost_text):
@@ -165,6 +174,20 @@ def cost_convert(cost_text):
     # convert cost to string with dot separator
     cost = "{:,}".format(cost)
     return cost
+
+def date_from_trans_date_time(trans_date_time, trans_time: bool = False):
+    # trans date time format: 14:31 Chủ Nhật 19/05/2024
+    # get last 10 characters
+    date = trans_date_time[-10:]
+    # current format: 19/05/2024 -> new format: 2024-05-19
+    date = date[-4:] + '-' + date[3:5] + '-' + date[:2]
+    if trans_time:
+        # get first 5 characters
+        time = trans_date_time[:5]
+        # current format: 14:31 -> new format: 14:31:00
+        time = time + ':00'
+        # combine date and time
+        date = date + ' ' + time
 
 def send_telegram(message):
     if len(message) > 4096:
