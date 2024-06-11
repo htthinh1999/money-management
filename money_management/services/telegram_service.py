49ffa2231e4166fde1f462ddd2eec9c527f0a33e
Thinh Huynh <thinh.tanhuynh@infodation.vn>
2024-06-11 16:25:22 +0700

    Fix cannot convert message report_detail

--- money_management/services/telegram_service.py
+++ money_management/services/telegram_service.py
@@ -118,6 +118,8 @@ def prepare_month_report_detail_message(daily_date: str, daily_group: list[Daily
     message = f"Chi tiêu ngày `{daily_date}`: <b>{total_amount}</b>\n"
     message = f"{message}{'-' * 41}\n"
     for daily in daily_group:
-        message = f"{message}{daily.time}{' ' * 5}{daily.amount:,}{' ' * 5}{daily.categorye}\n"
+        message = (
+            f"{message}{daily.time}{' ' * 5}{daily.amount:,}{' ' * 5}{daily.category}\n"
+        )
 
     return message
