14861a49535a2597246d410c5fd974719a221afd
Thinh Huynh <thinh.tanhuynh@infodation.vn>
2024-06-11 15:48:13 +0700

    Undo process

--- money_management/services/telegram_service.py
+++ money_management/services/telegram_service.py
@@ -43,12 +43,12 @@ def process_message(message):
     text: str = message["text"]
     if text == "/start":
         telegram.send_message("Chào bạn, mình là bot quản lý chi tiêu")
-    elif text.startswith("/report_detail"):
-        month = text.split(" ")[1] if len(text.split(" ")) > 1 else None
-        month_report_detail(month)
-    elif text.startswith("/report"):
-        month = text.split(" ")[1] if len(text.split(" ")) > 1 else None
-        month_report(month)
+    # elif text.startswith("/report_detail"):
+    #     month = text.split(" ")[1] if len(text.split(" ")) > 1 else None
+    #     month_report_detail(month)
+    # elif text.startswith("/report"):
+    #     month = text.split(" ")[1] if len(text.split(" ")) > 1 else None
+    #     month_report(month)
     else:
         telegram.send_message("Mình không hiểu bạn muốn gì, hãy thử lại")
 
