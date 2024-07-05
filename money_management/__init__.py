59f8e008873f1d09d1bf5600d07eb268e63e0742
Thinh Huynh <thinh.tanhuynh@infodation.vn>
2024-07-05 16:22:04 +0700

    Config log for GCP

--- money_management/__init__.py
+++ money_management/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from cloud_detect import provider
 import logging
 
 app = Flask(__name__)
+
+if provider() == "gcp":
+    # Set up the Google Cloud Logging python client library
+    import google.cloud.logging
+
+    client = google.cloud.logging.Client()
+    client.setup_logging()
+
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
