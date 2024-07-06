4140e93c608dd21cc0e9f2879de34325e7dd6f54
Thinh Huynh <thinh.tanhuynh@infodation.vn>
2024-07-06 10:06:25 +0700

    Update handler for GCP logging

--- money_management/__init__.py
+++ money_management/__init__.py
@@ -4,6 +4,24 @@ import logging
 
 app = Flask(__name__)
 
+
+def is_cloud_handler(handler: logging.Handler) -> bool:
+    """
+    is_cloud_handler
+
+    Returns True or False depending on whether the input is a
+    google-cloud-logging handler class
+
+    """
+    accepted_handlers = (
+        google.cloud.logging.handlers.StructuredLogHandler,
+        google.cloud.logging.handlers.CloudLoggingHandler,
+        google.cloud.logging.handlers.ContainerEngineHandler,
+        google.cloud.logging.handlers.AppEngineHandler,
+    )
+    return isinstance(handler, accepted_handlers)
+
+
 # Configure logging
 logging.basicConfig(
     level=logging.INFO,
@@ -14,7 +32,8 @@ if provider() == "gcp":
     import google.cloud.logging
 
     client = google.cloud.logging.Client()
-    logging.getLogger().handlers = [client.get_default_handler()]
+    root_logger = logging.getLogger()
+    root_logger.handlers = [h for h in root_logger.handlers if is_cloud_handler(h)]
     client.setup_logging()
 
 from money_management import database
