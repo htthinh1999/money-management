05a1440c251fd59795a2f60ac61d9cdd9ef1a160
Thinh Huynh <thinh.tanhuynh@infodation.vn>
2024-07-16 17:35:14 +0700

    Enable exactly once delivery

--- terraform/pubsub_money_management.tf
+++ terraform/pubsub_money_management.tf
@@ -5,14 +5,15 @@ module "pubsub_money_management" {
   project_id = var.project_id
   push_subscriptions = [
     {
-      name                  = "SUBSCRIPTION_PUSH_MONEY_MANAGEMENT"
-      push_endpoint         = "${local.money_management_url}/webhook"
-      max_delivery_attempts = 5
-      ack_deadline_seconds  = 20
-      x-goog-version        = "v1beta1"
-      expiration_policy     = ""
-      maximum_backoff       = "600s"
-      minimum_backoff       = "10s"
+      name                         = "SUBSCRIPTION_PUSH_MONEY_MANAGEMENT"
+      push_endpoint                = "${local.money_management_url}/webhook"
+      max_delivery_attempts        = 5
+      ack_deadline_seconds         = 20
+      x-goog-version               = "v1beta1"
+      expiration_policy            = ""
+      maximum_backoff              = "600s"
+      minimum_backoff              = "10s"
+      enable_exactly_once_delivery = true
     }
   ]
 }
